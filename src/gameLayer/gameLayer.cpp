2aa2ec8e3dfeb3188d4ceb1f00bdef4731ab3149
meemknight <vladalex420@gmail.com>
2023-07-27 00:10:25 +0300

    time stuff

--- src/gameLayer/gameLayer.cpp
+++ src/gameLayer/gameLayer.cpp
@@ -184,7 +184,7 @@ void gameStep(float deltaTime)
 			//server
 			if (f)
 			{
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+				std::this_thread::sleep_for(std::chrono::milliseconds(5));
 
 				if (followCurrentTurn)
 				{
